cd17b57e45fc0b0ecb24fa166a5d0593e4fae991
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-13 01:19:40 +0300

    Add files via upload

---
+++ 12. Defining Classes Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new("Dido", 31);       
+
+            Console.WriteLine ($"{person.Name} - {person.Age}");
+            
+        }
+    }
+}
