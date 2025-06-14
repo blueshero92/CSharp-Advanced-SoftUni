d67832213ee243597021c202b426dd888a04bc64
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 22:46:13 +0300

    Add files via upload

---
+++ 15. Iterators and Comparators Lab/01. Library/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        private string title;
+        private int year;
+        private List<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.title = title;
+            this.year = year;
+            this.authors = authors.ToList();
+        }
+
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        public List<string> Authors
+        {
+            get { return this.authors; }
+            set { this.authors = value; }
+        }
+    }
+
+
+}
