f60bbe29d26927d5c9ddf09a88378643a12d7542
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 22:44:20 +0300

    Add files via upload

---
+++ 15.Iterators and Comparators Lab/01. Library/Library.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in this.books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
