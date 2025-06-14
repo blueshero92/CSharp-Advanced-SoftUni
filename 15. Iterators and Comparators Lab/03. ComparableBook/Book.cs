98f415994a0316b204c7bc1d9e9e0698d57a6405
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 22:53:32 +0300

    Add files via upload

---
+++ 15. Iterators and Comparators Lab/03. ComparableBook/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators;
+public class Book : IComparable<Book>
+{
+    private string title;
+    private int year;
+    private List<string> authors;
+
+    public Book(string title, int year, params string[] authors)
+    {
+        this.title = title;
+        this.year = year;
+        this.authors = new(authors);
+    }
+
+    public string Title
+    {
+        get { return this.title; }
+        private set { this.title = value; }
+    }
+
+    public int Year
+    {
+        get { return this.year; }
+        private set { this.year = value; }
+    }
+
+    public IReadOnlyCollection<string> Authors
+    {
+        get { return this.authors.AsReadOnly(); }
+    }
+
+    public int CompareTo(Book other)
+    {
+        int result = this.Year.CompareTo(other.Year);
+
+        if (result == 0)
+        {
+            result = this.Title.CompareTo(other.Title);
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
