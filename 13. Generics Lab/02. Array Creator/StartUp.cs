79c1ae04a637d36745ab2deb50df2a571e531d96
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 09:00:46 +0300

    Add files via upload

---
+++ 13. Generics Lab/02. Array Creator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] array = ArrayCreator.Create(5, "Dido");
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
