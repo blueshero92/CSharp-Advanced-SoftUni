dabcf821e2dc0a2035b7d88025a4a8fb9362cd9a
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 22:39:12 +0300

    Add files via upload

---
+++ 14. Generics Exercise/07. Tuple/CustomTuple.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple
+{
+    internal class CustomTuple<T1, T2>
+    {
+        private T1 item1;
+        private T2 item2;
+
+        public CustomTuple(T1 item1, T2 item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public T1 Item1
+        {
+            get { return this.item1; }
+            set { this.item1 = value; }
+        }
+
+        public T2 Item2
+        {
+            get { return this.item2; }
+            set { this.item2 = value; }
+            
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Item1} -> {this.Item2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 14. Generics Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAdress = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] nameBeer = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] intDouble = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            CustomTuple<string, string> nameAndAdress = new ($"{nameAdress[0]} {nameAdress[1]}", nameAdress[2]);
+            CustomTuple<string, int> nameAndBeer = new(nameBeer[0], int.Parse(nameBeer[1]));
+            CustomTuple<int, double> intAndDouble = new(int.Parse(intDouble[0]) , double.Parse(intDouble[1]));
+
+            Console.WriteLine(nameAndAdress);
+            Console.WriteLine(nameAndBeer);
+            Console.WriteLine(intAndDouble);
+
+        }
+    }
+}
