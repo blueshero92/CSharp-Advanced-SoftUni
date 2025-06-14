7429ec95c44bf65b1916293e5dcd572ba0205535
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 22:58:09 +0300

    Add files via upload

---
+++ 16. Iterators and Comparators Exercise/01. ListyIterator/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> items = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
+
+            ListyIterator<string> listyIterator = new(items);
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            listyIterator.Print();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                }
+
+            }
+        }
+    }
+}
