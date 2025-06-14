240f2b057d666b2ba53fb994a24ee04622899c4a
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 09:11:32 +0300

    Add files via upload

---
+++ 14. Generics Exercise/03. Generic Swap Method String/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+
+                list.Add(input);
+            }
+
+            int[] indices = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Swap(indices[0], indices[1], list);
+
+            foreach (string name in list)
+            {
+                Console.WriteLine($"{name.GetType()}: {name.ToString()}");
+            }
+
+            void Swap<T>(int firstIndex, int secondIndex, List<T> list)
+            {
+                T temp = list[firstIndex];
+                list[firstIndex] = list[secondIndex];
+                list[secondIndex] = temp;
+            }
+        }
+    }
+}
