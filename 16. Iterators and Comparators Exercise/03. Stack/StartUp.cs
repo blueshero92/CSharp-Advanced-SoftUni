c4e5f27ca0472ab2778cd7df47be3bab368043f1
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 23:02:48 +0300

    Add files via upload

---
+++ 16. Iterators and Comparators Exercise/03. Stack/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> customStack = new();
+
+            string command;
+
+            while((command = Console.ReadLine()) != "END")
+            {
+                string[] tokens = command.Split(new char[] {' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string action = tokens[0];
+
+                if (action == "Push")
+                {
+                    int[] pushedItems = tokens.Skip(1).Select(int.Parse).ToArray();
+
+                    foreach (int item in pushedItems)
+                    {
+                        customStack.Push(item);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        customStack.Pop();
+                    }
+                    catch(InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            }
+
+            foreach (int item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
